bbdc1b8ca6f2f0e98d93b92c990c82fc77ec75ee
mamax49 <maxgillot@hotmail.fr>
2021-08-01 00:13:08 +0200

    reverse order name report

--- TheOtherRoles/Patches/MeetingPatch.cs
+++ TheOtherRoles/Patches/MeetingPatch.cs
@@ -363,7 +363,7 @@ namespace TheOtherRoles.Patches {
                 for (int i = 0; i < LogTrap.logTraps.Count; i++)
                 {
                     string msg = $"log trap {i + 1}:";
-
+                    LogTrap.logTraps[i].playersName.Reverse();
                     foreach (string playerName in LogTrap.logTraps[i].playersName)
                     {
                         msg += $" {playerName},";
